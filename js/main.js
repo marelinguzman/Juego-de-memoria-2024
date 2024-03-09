97d63cc39352051b0018965483d9bdc6644c41de
cristal <mcguzman@scl.edu.gt>
2024-03-08 19:22:14 -0600

    voltear_cartas

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
-let cards1 =["📜","🗺️","📻","🧤","📋","🗓️","🔒","📂","📉","💎","🎹","📈"]
-let cards2 =["📜","🗺️","📻","🧤","📋","🗓️","🔒","📂","📉","💎","🎹","📈"]
+let cards1 = ["🚀","🏍️","🚛","🚌","🚗","🚓","🚕","🚑","🛻","🛺","🛸","🛩️" ]
+let cards2 = ["🚀","🏍️","🚛","🚌","🚗","🚓","🚕","🚑","🛻","🛺","🛸","🛩️" ]
+let todas_las_cartas= cards1.concat(cards2)
 
 function repartir_Cartas(){
     let tablero = document.querySelector("#root")
-    let todas_las_cartas = cards1.concat(cards2);
     todas_las_cartas.forEach((cada_Carta)=>{
-        let cart = document.createElement("div")
-        cart.innerHTML = `<div class="carta">${cada_Carta}</div>`;
+        let carta = document.createElement("div")
+        carta.classList.add("carta_trasera")
+        carta.innerHTML = `<div class="carta_frontal">${cada_Carta}</div>`;
         //agrega un elemento hijo a este div, tiene que ser un createElement
-        tablero.appendChild(cart)
+        tablero.appendChild(carta)
     })
 }
 
@@ -16,3 +17,10 @@ function repartir_Cartas(){
 
 
 repartir_Cartas();
+
+let todas_las_cartad = document.querySelectorAll(".carta_trasera");
+    todas_las_cartad.forEach((cada_div)=>{
+        cada_div.addEventListener("click",()=>{
+            cada_div.classList.add("activar")
+        })
+    })
